f823b1f29818ccef1e00895d7c180b9749a87214
Woomin-Jeon <dal96k@gmail.com>
2022-03-09 17:17:43 +0900

    fix: Layout 컴포넌트 태그 및 스타일 수정

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,31 +1,36 @@
 import type { NextPage } from 'next'
 import Link from 'next/link'
+import React from 'react'
 
 const Home: NextPage = () => {
   return (
     <ul>
       <li>
         <Link href="/todo/csr" passHref>
-          <button style={{ padding: '8px 16px' }}>CSR TodoList {'>'}</button>
+          <button style={ButtonStyle}>CSR TodoList {'>'}</button>
         </Link>
       </li>
       <li>
         <Link href="/todo/ssr" passHref>
-          <button style={{ padding: '8px 16px' }}>SSR TodoList {'>'}</button>
+          <button style={ButtonStyle}>SSR TodoList {'>'}</button>
         </Link>
       </li>
       <li>
         <Link href="/todo/ssg" passHref>
-          <button style={{ padding: '8px 16px' }}>SSG TodoList {'>'}</button>
+          <button style={ButtonStyle}>SSG TodoList {'>'}</button>
         </Link>
       </li>
       <li>
         <Link href="/todo/ssg_revalidate" passHref>
-          <button style={{ padding: '8px 16px' }}>SSG TodoList - revalidate {'>'}</button>
+          <button style={ButtonStyle}>SSG TodoList - revalidate {'>'}</button>
         </Link>
       </li>
     </ul>
   )
+}
+
+const ButtonStyle: React.CSSProperties = {
+  padding: '8px 16px',
 }
 
 export default Home
--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -3,17 +3,41 @@ import React from 'react'
 
 const Layout: React.FC = ({ children }) => {
   return (
-    <main style={{ width: '400px', border: '1px solid black', borderRadius: '8px', padding: '16px', height: '600px' }}>
+    <>
       <Head>
         <title>NextJS-Redux Todo App</title>
         <meta name="description" content="nextjs redux todo app" />
       </Head>
 
-      <h1>NextJS-Redux Todo App</h1>
+      <main style={MainLayoutStyle}>
+        <header>
+          <h1>NextJS-Redux Todo App</h1>
+        </header>
 
-      {children}
-    </main>
+        {children}
+
+        <footer style={FooterLayoutStyle}>
+          <button style={BackButtonStyle}>{'<'} Back to home</button>
+        </footer>
+      </main>
+    </>
   )
+}
+
+const MainLayoutStyle: React.CSSProperties = {
+  position: 'relative',
+  width: '400px',
+  border: '1px solid black',
+  borderRadius: '8px',
+  padding: '16px',
+  height: '600px',
+}
+const FooterLayoutStyle: React.CSSProperties = {
+  position: 'absolute',
+  bottom: '16px',
+}
+const BackButtonStyle: React.CSSProperties = {
+  padding: '8px 16px',
 }
 
 export default Layout
